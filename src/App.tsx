e3a0a19545ccbc752815381ce0bee10d165a7b2a
takakowatabe <kikotkk@gmail.com>
2025-03-25 21:08:02 +0900

    今日の成果

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,70 @@
-import { Button, Box, Input, Heading } from '@chakra-ui/react';
+import { Record } from "./domain/record";
+import { Button, Box, Input, Heading, Flex, Table, Thead, Tbody,Tr,Th,Td, } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import React from 'react';
 import { supabase } from "./supabaseClient.js";
 
-const A = () => {
-  const [record, setRecord] = useState([]);
+const App = () => {
+  const [record, setRecord] = useState<Record[]>([]);
   const [time, setTime] = useState('');
   const [text, setText] = useState('');
   const [loeading, setLoeading] = useState([]);
   const [errorMessage, setErrorMessage] = useState([]);
 
-
   return (
-    <Box bg='gray.50' display="flex" flexDirection="column" alignItems="center" justifyContent="center" my="8" p="8">
-      <Heading as="h1" size="xl" my="4">学習アプリ</Heading>
-      <form>
-        学習内容<Input  my="2" bg='white'
-          type="text"
-          value={text}
-        />
-      </form>
-      <form>
-        学習時間<Input  my="2" bg='white'
-          type="number"
-          value={time}
-        />
-      </form>
-      <Button colorScheme="teal">保存</Button>
+  <Box w="100%" h="100%" p="8">
+    <Flex justify="center" align="center">
+      <Heading as="h1">学習アプリ</Heading>
+   </Flex>
+    <Box  gap={4} display="flex" justifyContent="center" alignItems="center" h="500px" p="8">
+      <Box w="400px" h="full" bg='gray.100' p="8">
+        <form>
+          日時
+          <Input  my="2" bg='white'
+            type="time"
+            value={time}
+          />
+        </form>
+        <form>
+          内容
+          <Input  my="2" bg='white'
+            type="text"
+            value={text}
+          />
+        </form>
+        <form>
+          時間
+          <Input  my="2" bg='white'
+            type="number"
+            value={time}
+          />
+        </form>
+        <Button colorScheme="teal" w="full" mt="8">保存</Button>
+      </Box>
+      <Box w="400px" p="8"  h="full" justifyContent="center" alignItems="center">
+        <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>日時</Th>
+            <Th>内容</Th>
+            <Th>時間</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {record.map((r) => (
+          <Tr key={r.id}>
+            <Td>{r.created_at}</Td>
+            <Td>{r.title}</Td>
+            <Td>{r.time}</Td>
+          </Tr>
+           ))}
+        </Tbody>
+      </Table>
+      </Box>
     </Box>
+  </Box>
   );
 };
 
-export default A;
+export default App;
+
