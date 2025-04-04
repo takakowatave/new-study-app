a5f46efe0f15d36adef8d1541f22a026880f069c
takakowatabe <kikotkk@gmail.com>
2025-04-04 11:29:06 +0900

    エラーリファクタ

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { StudyRecord, NewRecord } from "./domain/studyRecord";
-import { Button, Box, Input, Heading, Flex, Table, Thead, Tbody,Tr,Th,Td, } from '@chakra-ui/react';
+import { Button, Box, Input, Heading, Flex, Table, Thead, Tbody,Tr,Th,Td,  Modal,ModalOverlay,ModalContent, ModalHeader, ModalCloseButton,ModalBody,ModalFooter } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import React from 'react';
 import { supabase } from "./utils/supabase";
+import { useDisclosure } from '@chakra-ui/react';
+import { useForm } from "react-hook-form";
+
 
 const App = () => {
   const [records, setRecords] = useState<StudyRecord[]>([]);
@@ -11,6 +14,8 @@ const App = () => {
   const [createdAt, setCreatedAt] = useState('');
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
 
   const fetchRecords = async () => { // 非同期関数を定義
@@ -20,7 +25,9 @@ const App = () => {
       setErrorMessage(error.message); // エラーだったらメッセージ出す
       return;
     }
-    setRecords(data);
+    setRecords(data.map(d =>
+      StudyRecord.newRecord(d.id, d.title, d.time, d.created_at) //4つの引数（id, title, time, created_at）が必要
+    ));
     setLoading(false); // ローディングじゃない時
   };
   
@@ -30,7 +37,7 @@ const App = () => {
     if (text.trim()==="")  {
       setErrorMessage("学習内容を入力してください");
       return;  
-    }
+    };
     //newRecord(型)をオブジェクトとして定義
     const newRecord: NewRecord = {
       title: text,
@@ -77,60 +84,76 @@ const App = () => {
   <Box w="100%" h="100%" p="8">
     <Flex justify="center" align="center">
       <Heading as="h1">学習アプリ</Heading>
-  </Flex>
-    <Box gap={4} display="flex" justifyContent="center" alignItems="center" h="500px" p="8">
-      <Box w="400px" h="full" bg='gray.100' p="8">
-        <form>
-          日時
-          <Input  my="2" bg='white'
-            type="datetime-local"
-            value={createdAt}
-            onChange={(e) => setCreatedAt(e.target.value)}
-          />
-        </form>
-        <form>
-          内容
-          <Input  my="2" bg='white'
-            type="text"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-          />
-        </form>
-        <form>
-          時間
-          <Input  my="2" bg='white'
-            type="number"
-            value={time}
-            min="0"
-            onChange={(e) => setTime(e.target.value)}
-          />
-        </form>
-        <Button
-          onClick={handleAdd}
-          colorScheme="teal" w="full" mt="8">保存</Button>
-      </Box>
-      <Box w="400px" p="8"  h="full" justifyContent="center" alignItems="center">
-        <Table variant="simple">
-        <Thead>
-          <Tr>
-            <Th>日時</Th>
-            <Th>内容</Th>
-            <Th>時間</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {records.map((r) => (
-          <Tr key={r.id}>
-            <Td>{r.created_at}</Td>
-            <Td>{r.title}</Td>
-            <Td>{r.time}</Td>
-          </Tr>
-          ))}
-        </Tbody>
-      </Table>
-      </Box>
-    </Box>
+      <Button onClick={onOpen}>登録</Button>
+    </Flex>
+    <Modal isOpen={isOpen} onClose={onClose}>
+    <ModalOverlay />
+    <ModalContent>
+      <ModalHeader>登録確認</ModalHeader>
+      <ModalCloseButton />
+      <ModalBody>
+        <Box gap={4} display="flex" justifyContent="center" alignItems="center" h="500px" p="8">
+        <Box w="400px" h="full" bg='gray.100' p="8">
+          <form onSubmit={handleSubmit(handleAdd)}>
+            日時
+            <Input  my="2" bg='white'
+              type="datetime-local"
+              value={createdAt}
+              {...register("created_at", { required: "日時の入力は必須です" })}
+              onChange={(e) => setCreatedAt(e.target.value)}
+            />
+            {errors.createdAt && <Box color="red.500">{errors.createdAt.message}</Box>}
+
+            内容
+            <Input  my="2" bg='white'
+              type="text"
+              {...register("text", { required: "内容の入力は必須です"})}
+            />
+            {errors.text && <Box color="red.500">{errors.text.message}</Box>}
+            時間
+            <Input  my="2" bg='white'
+              type="number"
+              value={time}
+              {...register("time", {
+                required: "時間の入力は必須です",
+                min: { value: 1, message: "時間は0以上である必要があります"}
+              })}
+              onChange={(e) => setTime(e.target.value)}
+            />
+            {errors.time && <Box color="red.500">{errors.time.message}</Box>}
+            <Button type="submit" colorScheme="teal" w="full" mt="8">保存</Button>
+          </form>
+
+        </Box>
+        </Box>
+      </ModalBody>
+      <ModalFooter>
+        <Button onClick={onClose}>閉じる</Button>
+      </ModalFooter>
+    </ModalContent>
+  </Modal>
+  <Box w="400px" p="8"  h="full" justifyContent="center" alignItems="center">
+          <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>日時</Th>
+              <Th>内容</Th>
+              <Th>時間</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {records.map((r) => (
+            <Tr key={r.id}>
+              <Td>{r.created_at}</Td>
+              <Td>{r.title}</Td>
+              <Td>{r.time}</Td>
+            </Tr>
+            ))}
+          </Tbody>
+        </Table>
+        </Box>
   </Box>
+  
   );
 };
 
